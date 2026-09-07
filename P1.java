7e70cf48f74f98b8c2c3a8909b1ec13320b3b11e
Neel Singh <singh.neel2709@gmail.com>
2026-09-07 18:59:05 +0530

    Add files via upload

---
+++ P1.java
@@ -0,0 +1,10 @@
+//Running a simple java program to see its printing and looping syntax.
+
+class P1{
+    public static void main(String []args){
+        int i;
+        for (i=1; i<=10;i++){
+            System.out.println(i*4);
+        }
+    }
+}
