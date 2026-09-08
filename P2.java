772a810c3c8c935106ca82e7ae99f08be4be0e8b
Neel Singh <singh.neel2709@gmail.com>
2026-09-08 19:28:52 +0530

    Add files via upload

---
+++ P2.java
@@ -0,0 +1,10 @@
+// Printing the table for 4 from 1 to 10.
+
+public class P2{
+    public static void main(String[] args) {
+        int i;
+        for(i=1; i<=10; i++){
+            System.out.println("4 x "+i+" = "+4*i);
+        }
+    }
+}
