d0b480e28f450887f917a6b5eee7f8caceb3014c
Neel Singh <singh.neel2709@gmail.com>
2026-09-12 00:23:46 +0530

    Add files via upload

---
+++ P7.java
@@ -0,0 +1,9 @@
+// Testing multiple comma seperated for loop components.
+
+public class P7{
+    public static void main(String[] args) {
+        int i,j;
+        for(i=1,j=2; i<=10; i++,j=j+2)
+            System.out.println(i+" "+j);    //This is how to print two numbers together in java.
+    }
+}
