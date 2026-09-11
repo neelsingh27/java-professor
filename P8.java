60e0226613e216744b246b4b1dc3c7b4c16cfeba
Neel Singh <singh.neel2709@gmail.com>
2026-09-12 00:24:25 +0530

    Add files via upload

---
+++ P8.java
@@ -0,0 +1,11 @@
+// Learning while loop in java
+
+public class P8{
+    public static void main(String[] args) {
+        int i=1;
+        while (i<=10){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
