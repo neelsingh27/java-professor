8fbc14caf1536ca58e142b5749d850abbab435b5
Neel Singh <singh.neel2709@gmail.com>
2026-09-13 19:00:54 +0530

    Add files via upload

---
+++ P10.java
@@ -0,0 +1,27 @@
+// Understanding &&, &, || and |
+
+/*
+public class P10{
+    public static void main(String[] args) {
+        int i=10, j=20;
+        if (i>5 && j>10){
+            System.out.println("Greater");
+        }
+        else{
+            System.out.println("Less");
+        }
+    }
+}
+*/
+public class P10{
+    public static void main(String[] args) {
+        int i=10, j=20;
+        if (i>5 & j-->10){
+            System.out.println("Greater");
+        }
+        else{
+            System.out.println("Less");
+        }
+        System.out.println(j);
+    }
+}
---
+++ P9.java
@@ -0,0 +1,11 @@
+// Learning do-while loop in java.
+
+public class P9{
+    public static void main(String[] args) {
+        int i=1;
+        do { 
+            System.out.println(i);
+            i++;
+        } while (i<=10);
+    }
+}
