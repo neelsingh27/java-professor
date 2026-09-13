bcf31a836d526267601a48cbe4768635b8263a32
Neel Singh <singh.neel2709@gmail.com>
2026-09-13 19:12:03 +0530

    Add files via upload

---
+++ Extra-Practice/P5.java
@@ -0,0 +1,17 @@
+/*
+1 2 3 4
+2 3 4 5
+3 4 5 6
+4 5 6 7
+*/
+
+public class P5 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 4; j++) {
+                System.out.print(i + j - 1 + " ");
+            }
+            System.out.println();
+        }
+    }
+}
