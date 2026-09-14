eb12df4f95ba42b1952ddd9b223c99930b19418d
Neel Singh <singh.neel2709@gmail.com>
2026-09-14 19:03:36 +0530

    Add files via upload

---
+++ Basics/P11.java
@@ -0,0 +1,13 @@
+// Simple pattern problem
+
+public class P11{
+    public static void main(String[] args) {
+        int i,j;
+        for (i=1; i<=5; i++){
+            for (j=1; j<=i; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
